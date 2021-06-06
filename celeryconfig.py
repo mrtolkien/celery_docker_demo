547654cbee016855ea09ee006c7f6a5b47d17c92
mrtolkien <gary.mialaret@gmail.com>
2021-06-06 10:38:39 +0900

    Starting commit

---
+++ celeryconfig.py
@@ -0,0 +1,8 @@
+broker_url = "redis://redis"
+result_backend = "redis://redis"
+
+task_serializer = "json"
+result_serializer = "json"
+accept_content = ["json"]
+timezone = "UTC"
+enable_utc = True
